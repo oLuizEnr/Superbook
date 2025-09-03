302b0a6c9f6bff1dddee1c869c1883cfb1ab5e8c
Luiz Enrique <luizenrique80025030@gmail.com>
2025-09-03 16:28:22 -0300

    Templates com bootstrap feitos, 'base.html' e 'partials/menu.html' já criados no projeto com a alteração dos diretórios de templates.

--- superbook/settings.py
+++ superbook/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'superbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'superbook'/'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
