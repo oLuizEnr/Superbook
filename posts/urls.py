3c1f1200e2900ce0b62debcebd0507c5099dd57b
Luiz Enrique <luizenrique80025030@gmail.com>
2025-09-04 14:37:45 -0300

    Form, template, view e url de cadastro de post feitos.

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('lista/', views.lista_posts, name='lista_posts'),
-    path('cbv-lista/', views.PostListView.as_view(), name="cbv_lista_posts")
+    path('cbv-lista/', views.PostListView.as_view(), name="cbv_lista_posts"),
+    path('novo/', views.criar_post, name="criar_post"),
 ]
 
 # Luiz Enrique
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.views.generic import ListView
+from .forms import PostForm
 
 # Create your views here.
 def lista_posts(request):
     posts = Post.objects.all()
-    return render(request, "posts/lista_posts.html", {"postagens": posts})
+    return render(request,
+                  "posts/lista_posts.html",
+                  {"postagens": posts}
+    )
 
 class PostListView(ListView):
     model = Post
     template_name = "posts/lista_posts.html"
     context_object_name = "postagens"
+
+def criar_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_posts')
+    else:
+        form = PostForm()
+
+    return render(request,
+                  "posts/form_post.html",
+                  {"form": form}
+    )
     
 # Luiz Enrique
